7609b0248d626a3a70d759a43284ec2d0fdcc088
Anunay Anand <anunayanand@github.com>
2025-06-26 01:54:10 +0530

    Tic Tac Toe code optimized

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
-export default function Player ({ initialName, symbol }) {
+export default function Player ({ initialName, symbol, isActive, onChangeName }) {
     
     const [isEditing, setIsEditing] = useState(false);
     const [playerName, setPlayerName] = useState(initialName);
     
-    function handleEditClick() { setIsEditing(isEditing => !isEditing) };
+    function handleEditClick() { 
+        if(isEditing) {
+            onChangeName(symbol, playerName);
+        }
+        setIsEditing(isEditing => !isEditing)
+    };
+
     function handleChange(event) { setPlayerName(event.target.value) };
 
     return (
-        <li>
+        <li className={ isActive ? 'active': undefined }>
             <span className="player">
               {isEditing ? <input type="text" value={playerName} onChange={handleChange} required /> : <span className="player-name">{ playerName }</span>}
               <span className="player-symbol">{ symbol }</span>
